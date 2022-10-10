ded1d7c6382c1bf5b6fa806e1fff4dabdf93e428
amikhaleff <amikhaleff@gmail.com>
2022-10-10 10:59:52 +0300

    changed formatting code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
 ﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
 Console.Write("Введите номер порядковый номер задачи ");
+Console.ForegroundColor = ConsoleColor.White;
 int Task = int.Parse(Console.ReadLine() ?? "0");
 
 if (Task == 1) ZADACHA_47();
 else if (Task == 2) ZADACHA_50();
 else if (Task == 3) ZADACHA_52();
-else Console.WriteLine("Такой задачи нет в списке");
-
+else
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("Такой задачи нет в списке!!!");
+    Console.ResetColor();
+}
 void ZADACHA_47()
 {
     Console.Clear();
@@ -18,14 +24,14 @@ void ZADACHA_47()
         return number;
     }
 
-    void GetArray(double[,] matrix,double min,double max)
+    void GetArray(double[,] matrix, double min, double max)
     {
         for (int i = 0; i < matrix.GetLength(0); i++)
         {
             for (int j = 0; j < matrix.GetLength(1); j++)
             {
-                matrix[i, j] = Math.Round(new Random().NextDouble()*(max-min)+min,1);
-            } 
+                matrix[i, j] = Math.Round(new Random().NextDouble() * (max - min) + min, 1);
+            }
         }
     }
 
@@ -36,7 +42,7 @@ void ZADACHA_47()
         {
             for (int j = 0; j < matr.GetLength(1); j++)
             {
-                Console.Write($"{matr[i, j]}\t");
+                Console.Write("{0,7}", matr[i, j]);
             }
             Console.WriteLine();
         }
@@ -45,13 +51,13 @@ void ZADACHA_47()
 
     int m = ReadData("Введите количество строк m: ");
     int n = ReadData("Введите количество столбцов n: ");
-    double min= ReadData("Введите min: ");
-    double max=ReadData("Введите max: ");
+    double min = ReadData("Введите min: ");
+    double max = ReadData("Введите max: ");
     double[,] matrix = new double[m, n];
     Console.WriteLine();
-    GetArray(matrix,min,max);
+    GetArray(matrix, min, max);
     PrintArray(matrix);
-    System.Console.WriteLine( );
+    System.Console.WriteLine();
 }
 
 void ZADACHA_50()
@@ -84,7 +90,7 @@ void ZADACHA_50()
         {
             for (int j = 0; j < matr.GetLength(1); j++)
             {
-                Console.Write($"{matr[i, j]}\t");
+                Console.Write("{0,5}", matr[i, j]);
             }
             Console.WriteLine();
         }
@@ -133,37 +139,36 @@ void ZADACHA_52()
             for (int i = 0; i < arr.GetLength(0); i++)
             {
                 sum += arr[i, j];
-                
+
             }
-            
-            Console.Write(Math.Round((sum/arr.GetLength(1)),1) + "; ");
+
+            Console.Write("\nСреднее арифметическое {0}-го столбца = {1,5}", j + 1, Math.Round((sum / arr.GetLength(1)), 1) + ";");
 
 
         }
-        
+
     }
 
-void PrintArray(int[,] matr)
-{
-    for (int i = 0; i < matr.GetLength(0); i++)
+    void PrintArray(int[,] matr)
     {
-        for (int j = 0; j < matr.GetLength(1); j++)
+        for (int i = 0; i < matr.GetLength(0); i++)
         {
-            Console.Write($"{matr[i, j]}\t");
+            for (int j = 0; j < matr.GetLength(1); j++)
+            {
+                Console.Write("{0,5}", matr[i, j]);
+            }
+            Console.WriteLine();
         }
-        Console.WriteLine();
     }
-}
 
 
-int m = ReadData("Введите количество строк m: ");
-int n = ReadData("Введите количество столбцов n: ");
-int[,] matrix = new int[m, n];
-Console.WriteLine();
-GetArray(matrix);
-PrintArray(matrix);
-System.Console.Write("Среднее арифметическое каждого столбца: ");
-AvgColumsArray(matrix);
-System.Console.WriteLine();
+    int m = ReadData("Введите количество строк m: ");
+    int n = ReadData("Введите количество столбцов n: ");
+    int[,] matrix = new int[m, n];
+    Console.WriteLine();
+    GetArray(matrix);
+    PrintArray(matrix);
+    AvgColumsArray(matrix);
+    System.Console.WriteLine();
 }
 
